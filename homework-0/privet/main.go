9dc18724ef5bbb7cf162ba09a45ed10190894784
Eugene Klimov <tirava@gmail.com>
2019-04-20 15:33:44 +0300

    other tests

--- homework-0/privet/main.go
+++ homework-0/privet/main.go
@@ -7,5 +7,10 @@ import (
 
 func main() {
 	c := math.Sqrt(88)
-	fmt.Printf("%.1f", c)
+	fmt.Printf("%.1f\n", c)
+	fmt.Println("Address", &c)
+	//111
+	fmt.Println(len("Hello World"))
+	fmt.Println(" Hello World"[0])
+	fmt.Println("Hello " + "World")
 }
