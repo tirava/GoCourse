695e8c021deef86de37b152a76517c98f85a9d92
Eugene Klimov <tirava@gmail.com>
2019-04-21 13:10:56 +0300

    Конвертер валют

--- homework-1/main.go
+++ homework-1/main.go
@@ -1,1 +1,26 @@
-//111
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+const USD2RUB = 64
+
+func main() {
+	var sRub string
+	fmt.Println("\nУкажите количество рублей: ")
+	_, _ = fmt.Scanln(&sRub)
+	fRub, err := strconv.ParseFloat(sRub, 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fUsd := convertRub2Usd(fRub)
+	fmt.Printf("\nКонвертация в доллары:\n%.2f\n", fUsd)
+}
+
+func convertRub2Usd(fRub float64) float64 {
+	fUsd := fRub / USD2RUB
+	return fUsd
+}
