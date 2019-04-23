4c54ae112c69357b9735c98555aa4504579a4256
Eugene Klimov <tirava@gmail.com>
2019-04-23 09:55:48 +0300

    2. divisible by 3

--- homework-2/main.go
+++ homework-2/main.go
@@ -12,25 +12,38 @@ import (
 func main() {
 
 	// 1
-	var num int
-	fmt.Println("Enter integer number:")
-	_, err := fmt.Scanln(&num)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if isNumEven(num) {
+	msg := "\nEnter integer number fo check even/odd:"
+	num := getIntNum(&msg)
+	if isNumDivisibleBy(num, 2) {
 		fmt.Println("The number is even")
 	} else {
 		fmt.Println("The number is odd")
 	}
 
 	// 2
-
+	msg = "\nEnter integer number for check divisible by 3:"
+	num = getIntNum(&msg)
+	if isNumDivisibleBy(num, 3) {
+		fmt.Println("The number is divisible by 3")
+	} else {
+		fmt.Println("The number is not divisible by 3")
+	}
 }
 
-// isNumEven checks nums for even/odd
-func isNumEven(num int) bool {
-	if num%2 == 0 {
+// getIntNum return integer number from standard input
+func getIntNum(msg *string) int {
+	var num int
+	fmt.Println(*msg)
+	_, err := fmt.Scanln(&num)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return num
+}
+
+// isNumDivisibleBy checks if the number without remainder divisible by div
+func isNumDivisibleBy(num int, div int) bool {
+	if num%div == 0 {
 		return true
 	} else {
 		return false
