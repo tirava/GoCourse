86f4329f1946b21c7584f4a98d48eb945d64c8c8
Eugene Klimov <tirava@gmail.com>
2019-04-23 11:51:24 +0300

    3. Fibonacci numbers

--- homework-2/main.go
+++ homework-2/main.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 )
 
 func main() {
@@ -14,7 +15,8 @@ func main() {
 	// 1
 	msg := "\nEnter integer number fo check even/odd:"
 	num := getIntNum(&msg)
-	if isNumDivisibleBy(num, 2) {
+	result := isNumDivisibleBy(num, 2)
+	if result == 0 {
 		fmt.Println("The number is even")
 	} else {
 		fmt.Println("The number is odd")
@@ -23,11 +25,28 @@ func main() {
 	// 2
 	msg = "\nEnter integer number for check divisible by 3:"
 	num = getIntNum(&msg)
-	if isNumDivisibleBy(num, 3) {
-		fmt.Println("The number is divisible by 3")
+	result = isNumDivisibleBy(num, 3)
+	if result == 0 {
+		fmt.Println("The number is divisible by 3 without remainder")
 	} else {
-		fmt.Println("The number is not divisible by 3")
+		fmt.Println("The number is not divisible by 3, remainder:", result)
 	}
+
+	//3
+	msg = "\nEnter amount of the first Fibonacci numbers:"
+	num = getIntNum(&msg)
+	fmt.Println("First "+strconv.Itoa(num)+" Fibonacci numbers:\n", getNFibonacci(num, 1))
+}
+
+// getNFibonacci returns N first Fibonacci numbers in slice, first = 0 or 1
+func getNFibonacci(n int, first int) (slice []int) {
+	arr := make([]int, n)
+	arr[0] = first
+	arr[1] = 1
+	for i := 2; i < n; i++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr
 }
 
 // getIntNum return integer number from standard input
@@ -42,10 +61,11 @@ func getIntNum(msg *string) int {
 }
 
 // isNumDivisibleBy checks if the number without remainder divisible by div
-func isNumDivisibleBy(num int, div int) bool {
-	if num%div == 0 {
-		return true
+func isNumDivisibleBy(num int, div int) int {
+	result := num % div
+	if result == 0 {
+		return 0
 	} else {
-		return false
+		return result
 	}
 }
