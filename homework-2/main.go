01a5813c583e687d1b662e2f2a3e4264ff632593
Eugene Klimov <tirava@gmail.com>
2019-04-23 09:30:34 +0300

    1. even/odd func

--- homework-2/main.go
+++ homework-2/main.go
@@ -1,5 +1,38 @@
+// Syntax Go:	Homework-2
+// Author:		Eugene Klimov
+// Date:		23 april 2019
+
 package main
+
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 
+	// 1
+	var num int
+	fmt.Println("Enter integer number:")
+	_, err := fmt.Scanln(&num)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if isNumEven(num) {
+		fmt.Println("The number is even")
+	} else {
+		fmt.Println("The number is odd")
+	}
+
+	// 2
+
+}
+
+// isNumEven checks nums for even/odd
+func isNumEven(num int) bool {
+	if num%2 == 0 {
+		return true
+	} else {
+		return false
+	}
 }
