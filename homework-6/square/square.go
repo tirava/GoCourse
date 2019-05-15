ae46cb4d033c67a84c6cb0ad1d5565f2350f5048
Eugene Klimov <tirava@gmail.com>
2019-05-15 19:34:33 +0300

    Exercise #4 fixes

--- homework-6/square/square.go
+++ homework-6/square/square.go
@@ -8,11 +8,16 @@ import "math"
 
 // EquationSquare returns roots and discriminant
 func EquationSquare(a, b, c float64) (x1, x2, d float64) {
-	if a == 0 { // check if 1st arg must not be = 0
-		return 0, 0, 0
+	// check if 1st arg must not be = 0
+	if a == 0 {
+		// use sqrt(-1) to get NaN
+		x1 = math.Sqrt(-1)
+		x2, d = x1, x1
+		return x1, x2, d
 	}
 	// calc discriminant
 	d = b*b - 4*a*c
+
 	if d == 0 { // one root
 		x1 = -b / (2 * a) // no need calc common 'a2 := 2 * a' for best performance
 		x2 = x1
@@ -21,6 +26,8 @@ func EquationSquare(a, b, c float64) (x1, x2, d float64) {
 		ds := math.Sqrt(d) // common a2 & ds for best performance
 		x1 = (-b - ds) / a2
 		x2 = (-b + ds) / a2
-	} // if d < 0 then x1=x2=0
+	}
+	// if d < 0 then x1=x2=0 - no roots
+
 	return x1, x2, d
 }
--- homework-6/square/square_test.go
+++ homework-6/square/square_test.go
@@ -4,7 +4,10 @@
 // Date: 15 may 2019
 package square
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 type testValue struct {
 	a, b, c float64
@@ -12,6 +15,7 @@ type testValue struct {
 	x1, x2  float64
 }
 
+// testValues for testing square equation
 var testValues = []testValue{ // x1 is for minus calc in formula first, x2 is for plus in second time
 	{1, 0, 0, 0, 0, 0},
 	{1, 0, 1, -4, 0, 0},
@@ -21,13 +25,12 @@ var testValues = []testValue{ // x1 is for minus calc in formula first, x2 is fo
 	{123.45, 67.89, 10.11, -383.26590000000033, 0, 0}, // no roots
 }
 
-// TestDiscriminant test Discriminant only
+// TestDiscriminant tests discriminant only
 func TestDiscriminant(t *testing.T) {
 	for _, td := range testValues {
 		_, _, d := EquationSquare(td.a, td.b, td.c)
 		if d != td.d {
-			t.Error("For "+
-				"a =", td.a, ", b =", td.b, ", c =", td.c,
+			t.Error("For a =", td.a, ", b =", td.b, ", c =", td.c,
 				"expected discriminant =", td.d, "\nbut got", d)
 		}
 	}
@@ -36,10 +39,9 @@ func TestDiscriminant(t *testing.T) {
 // Test1stArg checks result if 'a=0' - equation impossible
 func Test1stArg(t *testing.T) {
 	x1, x2, d := EquationSquare(0, 0, 0)
-	if x1 != 0 || x2 != 0 || d != 0 {
-		t.Error("For "+
-			"a =", 0, ", b =", 0, ", c =", 0,
-			"expected 'x1 = 0, x2 = 0, d = 0' (ie impossible)\n but got",
+	if !math.IsNaN(x1) || !math.IsNaN(x2) || !math.IsNaN(d) {
+		t.Error("For a =", 0,
+			"expected 'x1 = NaN, x2 = NaN, d = NaN' (i.e. impossible)\n but got",
 			"x1 =", x1, ", x2 =", x2, ", d =", d)
 	}
 }
@@ -49,8 +51,7 @@ func TestRoots(t *testing.T) {
 	for _, tr := range testValues {
 		x1, x2, _ := EquationSquare(tr.a, tr.b, tr.c)
 		if x1 != tr.x1 || x2 != tr.x2 {
-			t.Error("For "+
-				"a =", tr.a, ", b =", tr.b, ", c =", tr.c,
+			t.Error("For a =", tr.a, ", b =", tr.b, ", c =", tr.c,
 				"expected x1 =", tr.x1, "x2 =", tr.x2, "\nbut got",
 				"x1 =", x1, ", x2 =", x2)
 		}
