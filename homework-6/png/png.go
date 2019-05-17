17d978a96856adced4b765514b3abb0043c39437
Eugene Klimov <tirava@gmail.com>
2019-05-17 15:38:49 +0300

    Exercise #2 - fix for png lines

--- homework-6/png/png.go
+++ homework-6/png/png.go
@@ -15,8 +15,8 @@ import (
 )
 
 const (
-	rectX    = 200 // box size
-	rectY    = 200
+	rectXY   = 300 // box size
+	stepXY   = 30  // step for both axis
 	fileName = "rectangle.png"
 )
 
@@ -25,12 +25,12 @@ func main() {
 	cyan := color.RGBA{R: 0, G: 255, B: 255, A: 255}
 	red := color.RGBA{R: 255, G: 0, B: 0, A: 255}
 
-	rectImg := image.NewRGBA(image.Rect(0, 0, rectX, rectY))
+	rectImg := image.NewRGBA(image.Rect(0, 0, rectXY, rectXY))
 	draw.Draw(rectImg, rectImg.Bounds(), &image.Uniform{C: cyan}, image.ZP, draw.Src)
 
 	// Draw lines
-	for y := 10; y < 200; y += 10 {
-		for x := 0; x < 200; x++ {
+	for y := stepXY; y < rectXY; y += stepXY {
+		for x := 0; x < rectXY; x++ {
 			rectImg.Set(x, y, red) // horizontal
 			rectImg.Set(y, x, red) // vertical
 		}
