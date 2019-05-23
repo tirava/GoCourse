eb24b6b26737e13c33a70195a3e82d967c6ba349
Eugene Klimov <tirava@gmail.com>
2019-05-23 13:27:53 +0300

    Exercise #5 - start & finish cars

--- homework-7/race/race.go
+++ homework-7/race/race.go
@@ -11,12 +11,18 @@ import (
 	"time"
 )
 
-const numOfCars = 3
+const (
+	numOfCars         = 3
+	distance  float64 = 1 // km
+	maxSpeed  float64 = 180
+	maxWait           = 10 // seconds
+)
 
 type car struct {
 	num   int           // car number
 	wait  time.Duration // car start ready time
-	speed int           // car speed
+	speed float64       // car speed in km/h
+	//time float64 // distance time
 }
 
 var wg sync.WaitGroup
@@ -25,29 +31,70 @@ func main() {
 	cars := make([]car, numOfCars)
 	rand.Seed(time.Now().UnixNano())
 
+	fmt.Println("Distance:", distance, "km")
+	fmt.Println("-------------------------------------------")
+
 	// fill start & speed
 	// add wait groups
 	// go all cars
 	for i := 0; i < numOfCars; i++ {
 		cars[i] = car{
 			num:   i + 1,
-			wait:  time.Duration(rand.Intn(10)),
-			speed: rand.Intn(180),
+			wait:  time.Duration(rand.Intn(maxWait)),
+			speed: rand.Float64() * maxSpeed,
 		}
 		wg.Add(1)
-		fmt.Println("Car #", i+1, " is waiting for start", int(cars[i].wait), "seconds...")
+		fmt.Println("Car #", i+1, "is waiting for start", int(cars[i].wait), "seconds...")
 		go cars[i].waitMe()
 	}
+	fmt.Println("-------------------------------------------")
 
 	//wait start for all cars
 	wg.Wait()
+	fmt.Println("-------------------------------------------")
 
+	// start all cars
+	for _, car := range cars {
+		fmt.Println("Car #", car.num, "is started...")
+		wg.Add(1)
+		go car.startMe()
+	}
+	fmt.Println("-------------------------------------------")
+
+	// wait cars finished
+	go spinner(50 * time.Millisecond)
+	wg.Wait()
+	fmt.Println("-------------------------------------------")
+
+	// check winner?
+	//
 }
 
-// waitCar method waits car for start
+// waitMe method waits car for start
 func (c car) waitMe() {
 	defer wg.Done()
 
 	time.Sleep(c.wait * time.Second)
 	fmt.Println("Car #", c.num, "is ready for start!")
+}
+
+// startMe method starts the car for distance km and fixes finish
+func (c car) startMe() {
+	defer wg.Done()
+
+	// get time for distance div speed and sleep )
+	t := distance / c.speed * 3600
+	time.Sleep(time.Duration(t) * time.Second)
+
+	fmt.Printf("Car # %d is finished for %.2f seconds! Speed: %.2f km/h\n", c.num, t, c.speed)
+}
+
+// spinner is for comfort waiting cars )
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range "-\\|/" {
+			fmt.Printf("%c\r", r)
+			time.Sleep(delay)
+		}
+	}
 }
